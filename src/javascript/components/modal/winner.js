868590c710f82988eb74975764e94588890c9506
Dnzln <qvanoman@gmail.com>
2021-01-18 20:52:59 +0200

    Add showWinnerModal

--- src/javascript/components/modal/winner.js
+++ src/javascript/components/modal/winner.js
@@ -1,5 +1,12 @@
-import { showModal } from './modal'
+import { showModal } from './modal';
+import { createFighterImage } from '../fighterPreview';
+
 
 export function showWinnerModal(fighter) {
-  console.log(fighter)
+    const winnerImageElement = createFighterImage(fighter);
+    const winnerData = {
+        title: fighter.name,
+        bodyElement: winnerImageElement,
+    }
+    showModal(winnerData);
 }
